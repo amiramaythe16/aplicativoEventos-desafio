743f9be4e5d27f8425406aa6b9c3476caa5322cf
Amira Maythe <amiramaythe@hotmail.com>
2022-04-19 12:11:18 -0300

    adicionado nova view de detalhes do evento

---
+++ Aplicativo Eventos - Desafio/Aplicativo Eventos - Desafio/View/TelaDetalheEventoView.swift
@@ -0,0 +1,25 @@
+//
+//  TelaDetalheEventoView.swift
+//  Aplicativo Eventos - Desafio
+//
+//  Created by cedesbr on 19/04/22.
+//
+
+import UIKit
+
+class TelaDetalheEventoView: UIView, CodableView {
+    func setupComponents() {
+        
+    }
+    
+    func buildViews() {
+        
+    }
+    
+    func setupConstraints() {
+        
+    }
+    
+
+
+}
